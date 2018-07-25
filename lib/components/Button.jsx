4fde0fcaf1afce404ace37a2da83e7e54383f6e5
Renzo Canepa <rcanepag@gmail.com>
2018-07-25 18:59:12 -0400

    components: add prop types to Button

--- lib/components/Button.jsx
+++ lib/components/Button.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = () => <button type="button">Hello</button>;
+const Button = ({ label }) => <button type="button">{label}</button>;
+
+Button.propTypes = {
+  label: PropTypes.string,
+};
+
+Button.defaultProps = {
+  label: 'Click me!',
+};
+
 export default Button;
